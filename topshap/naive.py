356657cf1d5052100f4ddd9329a1081e5fea25f6
Guangyi <zhangguangyi@sztu.edu.cn>
2025-02-27 16:13:17 +0800

    LSH

--- topshap/naive.py
+++ topshap/naive.py
@@ -1,6 +1,67 @@
 import numpy as np
+import itertools
 
 from topshap.helper import distance, kernel_value
+
+
+class LSH:
+    def __init__(self, n_tables, n_bits):
+        self.n_tables = n_tables
+        self.n_bits = n_bits
+
+        self.hash_tables = []
+        self.random_vecs = []
+
+    def build_hash_tables(self, X):
+        for _ in range(self.n_tables):
+            # Generate random projection vectors
+            vecs = np.random.randn(self.n_bits, X.shape[1])
+            self.random_vecs.append(vecs)
+            
+            # Create hash table for this projection
+            table = {}
+            for idx, x in enumerate(X):
+                hash_key = tuple((x @ vecs.T > 0).astype(int))  # Compute hash bits
+                if hash_key not in table:
+                    table[hash_key] = []
+                table[hash_key].append(idx)
+            self.hash_tables.append(table)
+
+    def query(self, x, n_nb):
+        """
+        Query hash tables to find candidate neighbors.
+        Probe nearby hash keys to find more up to n_nb candidates.
+        """
+        candidates = set()
+        
+        # Query hash tables to find candidate neighbors
+        for table, vecs in zip(self.hash_tables, self.random_vecs):
+            hash_key = tuple((x @ vecs.T > 0).astype(int))
+            if hash_key in table:
+                candidates.update(table[hash_key])
+
+        if len(candidates) >= n_nb:
+            return candidates
+
+        # Probe nearby hash keys to find more up to n_nb candidates
+        for nflipped in range(1, self.n_bits+1): # try all combinations of flipping bits
+            for table, vecs in zip(self.hash_tables, self.random_vecs):
+                hash_key = tuple((x @ vecs.T > 0).astype(int))
+                for comb in itertools.combinations(range(self.n_bits), nflipped):
+                    hash_key_comb = list(hash_key)
+                    for i in comb:
+                        hash_key_comb[i] = 1 - hash_key_comb[i]
+                    hash_key_comb = tuple(hash_key_comb)
+                    if hash_key_comb in table:
+                        candidates.update(table[hash_key_comb])
+
+                if len(candidates) >= n_nb:
+                    break
+
+            if len(candidates) >= n_nb:
+                break
+
+        return candidates
 
 
 def shapley_bf(D, Z_test, K, sigma):
